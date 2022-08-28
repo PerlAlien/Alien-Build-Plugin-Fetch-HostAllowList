a6c2ecf7af6dd00bf6201c301076d74ae4bb6837
Graham Ollis <plicease@cpan.org>
2022-08-28 11:03:46 -0600

    initial implementation

---
+++ README.md
@@ -0,0 +1,14 @@
+# Alien::Build::Plugin::Fetch::HostAllowList ![static](https://github.com/PerlAlien/Alien-Build-Plugin-Fetch-HostAllowList/workflows/static/badge.svg) ![linux](https://github.com/PerlAlien/Alien-Build-Plugin-Fetch-HostAllowList/workflows/linux/badge.svg)
+
+Require that Alien::Build based aliens only fetch from an allow list of hosts
+
+# AUTHOR
+
+Graham Ollis <plicease@cpan.org>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2022 by Graham Ollis.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
--- lib/Alien/Build/Plugin/Fetch/HostAllowList.pm
+++ lib/Alien/Build/Plugin/Fetch/HostAllowList.pm
@@ -3,9 +3,42 @@ package Alien::Build::Plugin::Fetch::HostAllowList;
 use strict;
 use warnings;
 use 5.008004;
+use Alien::Build::Plugin;
+use URI;
 
 # ABSTRACT: Require that Alien::Build based aliens only fetch from an allow list of hosts
 # VERSION
+
+has allow_hosts => sub { [] };
+
+sub init
+{
+  my($self, $meta) = @_;
+
+  my %allowed = map { $_ => 1 } @{ $self->allow_hosts };
+
+  $meta->around_hook( fetch => sub {
+    my $orig = shift;
+    my $build = shift;
+    my $url = $_[0] || $build->meta_prop->{start_url};
+
+    # If URL doesn't have a : then it doesn't have a scheme or
+    # protocol and we assume that it is a file or directory.
+    if($url =~ /:/)
+    {
+      my $url = URI->new($url);
+      if($url->scheme ne 'file')
+      {
+        local $@ = '';
+        my $host = eval { $url->host };
+        die "unable to determine host from $url: $@" if $@;
+        die "The host $host is not in the allow list" unless $allowed{$host};
+      }
+    }
+
+    $orig->($build, @_);
+  });
+}
 
 1;
 
