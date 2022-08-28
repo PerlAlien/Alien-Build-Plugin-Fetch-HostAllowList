8449d09ccb8ef8ecefd20fc94f2a520465663be4
Graham Ollis <plicease@cpan.org>
2022-08-28 11:00:46 -0600

    Initial structure

---
+++ lib/Alien/Build/Plugin/Fetch/HostAllowList.pm
@@ -0,0 +1,12 @@
+package Alien::Build::Plugin::Fetch::HostAllowList;
+
+use strict;
+use warnings;
+use 5.008004;
+
+# ABSTRACT: Require that Alien::Build based aliens only fetch from an allow list of hosts
+# VERSION
+
+1;
+
+
---
+++ t/alien_build_plugin_fetch_hostallowlist.t
@@ -0,0 +1,8 @@
+use Test2::V0 -no_srand => 1;
+use Alien::Build::Plugin::Fetch::HostAllowList;
+
+ok 1, 'todo';
+
+done_testing;
+
+
